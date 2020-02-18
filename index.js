6c3b82123ca7c662c7e36b6c40b7598e5c055566
Sheila R Moore <sherexmykes@github.com>
2020-02-17 22:46:19 -0500

    message of the day

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ require("dotenv").config();
 const server = require("./server.js");
 const port = process.env.PORT;
 
+server.get('/', async (req, res) => {
+  try {
+    const shoutouts = await db('posts');
+    const messageOfTheDay = process.env.MOTD || 'Hello User!'; // add this line
+    res.status(200).json({ motd: messageOfTheDay, shoutouts }); // change this line
+  } catch (error) {
+    console.error('\nERROR', error);
+    res.status(500).json({ error: 'Cannot retrieve the hello' });
+  }
+});
+
 server.listen(port, () => {
   console.log(`*** listening on port ${port}`);
 });
