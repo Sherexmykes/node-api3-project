f88b6087b31b387854bb5d3672d3661580b18e14
Sheila R Moore <sherexmykes@github.com>
2020-02-17 22:21:43 -0500

    add start

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 
 const server = require("./server.js");
-const port = process.env.PORT || 3100;
+const port = process.env.PORT;
 
 server.listen(port, () => {
   console.log(`*** listening on port ${port}`);
