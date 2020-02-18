c2643c3681547c75fca5030d6f221916723b39ea
Sheila R Moore <sherexmykes@github.com>
2020-02-17 20:44:58 -0500

    mvp

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code away!
+require("dotenv").config();
+
+
+
+const server = require("./server.js");
+const port = process.env.PORT || 3100;
+
+server.listen(port, () => {
+  console.log(`*** listening on port ${port}`);
+});
--- server.js
+++ server.js
@@ -1,13 +1,50 @@
-const express = require('express');
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const userRouter = require("./users/userRouter.js");
+const postRouter = require("./posts/postRouter");
+
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+//=======Global Middleware========================
+server.use(cors());
+const middleware = [express.json(), helmet(), morgan("dev")];
+const customMiddleware = [logger];
+server.use(middleware, customMiddleware);
+
+server.use("/api/users", userRouter);
+server.use("/api/posts", postRouter);
+
+//=======end of Global Middleware=================
+
+const motd = process.env.MOTD;
+server.get("/", (req, res) => {
+  res.send(`
+  <h2 style="text-align: center; color: blue">${motd}</h2>
+  <p style="text-align: center; color: blue">Click to see the api <a href="/api/users">API Call</a></p>
+`);
 });
 
 //custom middleware
+/*
+logger()
+logger logs to the console the following information about each request: request method, request url, and a timestamp
+this middleware runs on every request made to the API
+*/
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const URL = req.protocol + ":" + "//" + req.get("host") + "/" + req.path;
+
+  console.log(`Request Method: ${
+    req.method
+  }, URL: ${URL}, timestamp: ${new Date().toISOString()}
+  `);
+
+  next();
+}
 
 module.exports = server;
